6a23aa895e1aeee7e5025c45dda9ecfbbceffa0e
Olga Aleshchenko <mail_olga@inbox.ru>
2020-02-02 01:46:38 +0300

    task 7 without task 4

--- js/script5.js
+++ js/script5.js
@@ -1,1 +1,8 @@
 // Задание 5
+
+let str1 = '123',
+result;
+
+result= +str1[0] + +str1[1] + +str1[2];
+
+console.log(result);
--- js/script7.js
+++ js/script7.js
@@ -1,1 +1,13 @@
 // Задание 7
+
+a = 'test';
+function checkA(){
+a == 'test' ? console.log('Верно') : console.log('Неверно');
+}
+checkA();
+
+a = 'тест';
+checkA();
+
+a = 3;
+checkA();
