140497cc2dbcaef155a025201a0f8d5ec15ac5e1
Olga Aleshchenko <mail_olga@inbox.ru>
2020-02-02 00:39:39 +0300

    task 2

--- js/script1.js
+++ js/script1.js
@@ -12,3 +12,5 @@ num -= 18;
 num *= 10;
 num /= 15;
 console.log(num);
+
+
---
+++ js/script2.js
@@ -0,0 +1,7 @@
+// Задание 2
+
+let hour = 13,
+    minute= 15,
+    second = 33;
+
+alert(hour + ":" + minute + ":" + second + "'");
